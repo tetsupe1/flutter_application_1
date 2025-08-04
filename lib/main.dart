9eb35349b3fc0ffb4f9603395a248cec91cda554
tetsupe1 <sasaki.3063@gmail.com>
2025-08-04 11:49:58 +0900

    Icon配置

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/my_flutter_app_icons.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +12,54 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/home.jpg'),
-              fit: BoxFit.cover,
+        backgroundColor: Color.fromARGB(255, 0, 0, 0),
+        body: Column(
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/home1.jpg'),
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+              ),
             ),
-          ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      MyFlutterApp.fish,
+                      size: 50,
+                      color: Color.fromARGB(255, 155, 155, 155),
+                    ),
+                  ),
+                  SizedBox(width: 24),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.add,
+                      size: 50,
+                      color: Color.fromARGB(255, 155, 155, 155),
+                    ),
+                  ),
+                  SizedBox(width: 24),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.trending_up_outlined,
+                      size: 50,
+                      color: Color.fromARGB(255, 155, 155, 155),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
